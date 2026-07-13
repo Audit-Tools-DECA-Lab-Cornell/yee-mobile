e6e8cd660c907532013cb9a9c4f52cb3c6dac08a
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-07-13 16:58:18 -0400

    update bun version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "Youth Enabling Environments Audit Tool",
         slug: "audit-tools-yee-mobile",
-        version: "0.8.1",
+        version: "0.8.2",
         orientation: "portrait",
         icon: "./assets/icon.png",
         scheme: "yee-mobile",
